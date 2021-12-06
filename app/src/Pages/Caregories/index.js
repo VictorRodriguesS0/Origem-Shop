8f4a56ab4bf6f81edcaf2173b98e5774552ac62d
Victor Rodrigues <victorrsilva17@gmail.com>
2021-12-06 16:47:41 -0300

    Creating routes

---
+++ app/src/Pages/Caregories/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Categories = () => {
+  return (
+    <div>
+      <a>aaaaaaaaaaaaa</a>
+      <Link to="/">Categories</Link>
+    </div>
+  );
+};
+
+export default Categories;
